0beac3c5148406f38cfce370fbeef22cb9f4e0a3
“Anuj Sharma <anuj.k.sharma001@gmail.com>
2023-05-23 09:40:32 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void Main() {
         int a = 1;
         int b = 2;
-        System.out.println(a+b);
+        System.out.println("The Sum is: " + a+b);
     }
 }
